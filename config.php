5b465da23a67d65871cb021bd7f3e7670b0616a3
George Falkovich <gfalk85@gmail.com>
2024-11-02 11:04:01 +0200

    edits

--- config.php
+++ config.php
@@ -1,13 +1,7 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: georgefalkovich
- * Date: 08/01/2020
- * Time: 20:48
- */
 
-
-define("DB_HOST", "localhost");
-define("DB_NAME", "portfolio");
-define("DB_USER", "admin_user");
-define("DB_PASSWORD", "admin_password");
+// fake
+define("DB_HOST", "db host");
+define("DB_NAME", "db name");
+define("DB_USER", "db username");
+define("DB_PASSWORD", "db password");
