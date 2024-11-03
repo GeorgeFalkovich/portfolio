6cbda6a63ef7d59dabcedcf401eb74d0e99bfa0c
George Falkovich <gfalk85@gmail.com>
2024-11-03 21:05:31 +0200

    db changes

--- config.php
+++ config.php
@@ -1,6 +1,11 @@
 <?php
 
-// fake
+
+$DB_HOST=getenv('DB_HOST');
+$DB_NAME=getenv('DB_NAME');
+$DB_USER=getenv('DB_USER');
+$DB_PASSWORD=getenv('DB_PASSWORD');
+
 define("DB_HOST", "db host");
 define("DB_NAME", "db name");
 define("DB_USER", "db username");
